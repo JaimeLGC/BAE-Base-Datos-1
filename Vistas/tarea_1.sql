da25c57df41d254c6c4924f7414ba1e3285f59b4
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-05-09 23:00:43 +0100

    vistas 1

--- Vistas/tarea_1.sql
+++ Vistas/tarea_1.sql
@@ -3023,7 +3023,23 @@ select a.first_name, f.title from actor as a join film_actor as fa join film as 
 +------------+-----------------------+
 */
 --         Mostrar el nombre de la película y el de sus categorías.
-
+select f.title, c.name from film as f join film_category as fc join category as c on c.category_id = fc.category_id and fc.film_id = f.film_id limit 10;
+/*
++---------------------+--------+
+| title               | name   |
++---------------------+--------+
+| AMADEUS HOLY        | Action |
+| AMERICAN CIRCUS     | Action |
+| ANTITRUST TOMATOES  | Action |
+| ARK RIDGEMONT       | Action |
+| BAREFOOT MANCHURIAN | Action |
+| BERETS AGENT        | Action |
+| BRIDE INTRIGUE      | Action |
+| BULL SHAWSHANK      | Action |
+| CADDYSHACK JEDI     | Action |
+| CAMPUS REMEMBER     | Action |
++---------------------+--------+
+*/
 --         Mostrar el country, la ciudad y dirección de cada miembro del staff.
 
 --         Mostrar el country, la ciudad y dirección de cada customer.
@@ -3041,54 +3057,110 @@ select a.first_name, f.title from actor as a join film_actor as fa join film as 
 -- -- View structure for view `customer_list`
 -- --
 
--- CREATE VIEW customer_list AS
--- SELECT 
---   cu.customer_id AS ID, 
---     CONCAT(cu.first_name, _utf8mb4' ', cu.last_name) AS name, 
---     a.address AS address, 
---     a.postal_code AS `zip code`,
---   a.phone AS phone, 
---     city.city AS city, 
---     country.country AS country, 
---     IF(cu.active, _utf8mb4'active',_utf8mb4'') AS notes, 
---     cu.store_id AS SID
--- FROM 
---   customer AS cu JOIN address AS a 
---     ON cu.address_id = a.address_id 
---     JOIN city 
---     ON a.city_id = city.city_id
---   JOIN country 
---     ON city.country_id = country.country_id;
--- --
--- -- View structure for view `film_list`
--- --
+CREATE VIEW customer_list AS
+SELECT 
+    cu.customer_id AS ID, 
+    CONCAT(cu.first_name, _utf8mb4' ', cu.last_name) AS name, 
+    a.address AS address, 
+    a.postal_code AS `zip code`,
+    a.phone AS phone, 
+    city.city AS city, 
+    country.country AS country, 
+    IF(cu.active, _utf8mb4'active',_utf8mb4'') AS notes, 
+    cu.store_id AS SID
+FROM 
+    customer AS cu JOIN address AS a 
+    ON cu.address_id = a.address_id 
+    JOIN city 
+    ON a.city_id = city.city_id
+    JOIN country 
+    ON city.country_id = country.country_id;
 
--- CREATE VIEW film_list AS
--- SELECT 
---   film.film_id AS FID, 
---     film.title AS title, 
---     film.description AS description, 
---     category.name AS category, 
---     film.rental_rate AS price,
---   film.length AS length, 
---     film.rating AS rating, 
---     GROUP_CONCAT(CONCAT(actor.first_name, _utf8mb4' ', actor.last_name) SEPARATOR ', ') AS actors
--- FROM 
---   category LEFT JOIN film_category 
---     ON category.category_id = film_category.category_id 
---     LEFT JOIN film 
---     ON film_category.film_id = film.film_id
---   JOIN film_actor 
---     ON film.film_id = film_actor.film_id
---   JOIN actor 
---     ON film_actor.actor_id = actor.actor_id
--- GROUP BY film.film_id, category.name;
+
+CREATE VIEW film_list AS
+SELECT 
+    film.film_id AS FID, 
+    film.title AS title, 
+    film.description AS description, 
+    category.name AS category, 
+    film.rental_rate AS price,
+    film.length AS length, 
+    film.rating AS rating, 
+    GROUP_CONCAT(CONCAT(actor.first_name, _utf8mb4' ', actor.last_name) SEPARATOR ', ') AS actors
+FROM 
+    category LEFT JOIN film_category 
+    ON category.category_id = film_category.category_id 
+    LEFT JOIN film 
+    ON film_category.film_id = film.film_id
+    JOIN film_actor 
+    ON film.film_id = film_actor.film_id
+    JOIN actor 
+    ON film_actor.actor_id = actor.actor_id
+GROUP BY film.film_id,  category.name; 
 
 -- Conociendo es estos momentos ya la BBDD, se pide:
 
 --     Muestra el resultado de la consulta de las vistas que se proporcionan.
+
+select * from customer_list limit 10;
+/*
++-----+------------------+-------------------------------+----------+--------------+-----------------+----------------+--------+-----+
+| ID  | name             | address                       | zip code | phone        | city            | country        | notes  | SID |
++-----+------------------+-------------------------------+----------+--------------+-----------------+----------------+--------+-----+
+| 218 | VERA MCCOY       | 1168 Najafabad Parkway        | 40301    | 886649065861 | Kabul           | Afghanistan    | active |   1 |
+| 441 | MARIO CHEATHAM   | 1924 Shimonoseki Drive        | 52625    | 406784385440 | Batna           | Algeria        | active |   1 |
+|  69 | JUDY GRAY        | 1031 Daugavpils Parkway       | 59025    | 107137400143 | Béchar          | Algeria        | active |   2 |
+| 176 | JUNE CARROLL     | 757 Rustenburg Avenue         | 89668    | 506134035434 | Skikda          | Algeria        | active |   1 |
+| 320 | ANTHONY SCHWAB   | 1892 Nabereznyje Telny Lane   | 28396    | 478229987054 | Tafuna          | American Samoa | active |   2 |
+| 528 | CLAUDE HERZOG    | 486 Ondo Parkway              | 35202    | 105882218332 | Benguela        | Angola         | active |   1 |
+| 383 | MARTIN BALES     | 368 Hunuco Boulevard          | 17165    | 106439158941 | Namibe          | Angola         | active |   1 |
+| 381 | BOBBY BOUDREAU   | 1368 Maracabo Boulevard       | 32716    | 934352415130 | South Hill      | Anguilla       | active |   2 |
+| 359 | WILLIE MARKHAM   | 1623 Kingstown Drive          | 91299    | 296394569728 | Almirante Brown | Argentina      | active |   2 |
+| 560 | JORDAN ARCHULETA | 1229 Varanasi (Benares) Manor | 40195    | 817740355461 | Avellaneda      | Argentina      | active |   1 |
++-----+------------------+-------------------------------+----------+--------------+-----------------+----------------+--------+-----+
+*/
+
+select * from film_list limit 10;
+/*
++-----+------------------+-----------------------------------------------------------------------------------------------------------------------+-------------+-------+--------+--------+------------------------------------------------------------------------------------------------------------------------------------------------+
+| FID | title            | description                                                                                                           | category    | price | length | rating | actors                                                                                                                                         |
++-----+------------------+-----------------------------------------------------------------------------------------------------------------------+-------------+-------+--------+--------+------------------------------------------------------------------------------------------------------------------------------------------------+
+|   1 | ACADEMY DINOSAUR | A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies                      | Documentary |  0.99 |     86 | PG     | CHRISTIAN GABLE, LUCILLE TRACY, SANDRA PECK, JOHNNY CAGE, MENA TEMPLE, WARREN NOLTE, OPRAH KILMER, ROCK DUKAKIS, MARY KEITEL, PENELOPE GUINESS |
+|   2 | ACE GOLDFINGER   | A Astounding Epistle of a Database Administrator And a Explorer who must Find a Car in Ancient China                  | Horror      |  4.99 |     48 | G      | CHRIS DEPP, SEAN GUINESS, MINNIE ZELLWEGER, BOB FAWCETT                                                                                        |
+|   3 | ADAPTATION HOLES | A Astounding Reflection of a Lumberjack And a Car who must Sink a Lumberjack in A Baloon Factory                      | Documentary |  2.99 |     50 | NC-17  | NICK WAHLBERG, BOB FAWCETT, CAMERON STREEP, RAY JOHANSSON, JULIANNE DENCH                                                                      |
+|   4 | AFFAIR PREJUDICE | A Fanciful Documentary of a Frisbee And a Lumberjack who must Chase a Monkey in A Shark Tank                          | Horror      |  2.99 |    117 | G      | JODIE DEGENERES, SCARLETT DAMON, KENNETH PESCI, FAY WINSLET, OPRAH KILMER                                                                      |
+|   5 | AFRICAN EGG      | A Fast-Paced Documentary of a Pastry Chef And a Dentist who must Pursue a Forensic Psychologist in The Gulf of Mexico | Family      |  2.99 |    130 | G      | DUSTIN TAUTOU, THORA TEMPLE, MATTHEW CARREY, MATTHEW LEIGH, GARY PHOENIX                                                                       |
+|   6 | AGENT TRUMAN     | A Intrepid Panorama of a Robot And a Boy who must Escape a Sumo Wrestler in Ancient China                             | Foreign     |  2.99 |    169 | PG     | KIRSTEN PALTROW, SANDRA KILMER, JAYNE NEESON, WARREN NOLTE, MORGAN WILLIAMS, KENNETH HOFFMAN, REESE WEST                                       |
+|   7 | AIRPLANE SIERRA  | A Touching Saga of a Hunter And a Butler who must Discover a Butler in A Jet Boat                                     | Comedy      |  4.99 |     62 | PG-13  | RICHARD PENN, MICHAEL BOLGER, MENA HOPPER, OPRAH KILMER, JIM MOSTEL                                                                            |
+|   8 | AIRPORT POLLOCK  | A Epic Tale of a Moose And a Girl who must Confront a Monkey in Ancient India                                         | Horror      |  4.99 |     54 | R      | FAY KILMER, GENE WILLIS, SUSAN DAVIS, LUCILLE DEE                                                                                              |
+|   9 | ALABAMA DEVIL    | A Thoughtful Panorama of a Database Administrator And a Mad Scientist who must Outgun a Mad Scientist in A Jet Boat   | Horror      |  2.99 |    114 | PG-13  | WARREN NOLTE, GRETA KEITEL, WILLIAM HACKMAN, MERYL ALLEN, CHRISTIAN GABLE, MENA TEMPLE, RIP WINSLET, ELVIS MARX, RIP CRAWFORD                  |
+|  10 | ALADDIN CALENDAR | A Action-Packed Tale of a Man And a Lumberjack who must Reach a Feminist in Ancient China                             | Sports      |  4.99 |     63 | NC-17  | ALEC WAYNE, JUDY DEAN, VAL BOLGER, RAY JOHANSSON, RENEE TRACY, JADA RYDER, GRETA MALDEN, ROCK DUKAKIS                                          |
++-----+------------------+-----------------------------------------------------------------------------------------------------------------------+-------------+-------+--------+--------+------------------------------------------------------------------------------------------------------------------------------------------------+
+*/
+
 --     Crea 5 vistas sobre la BBDD, y realizar la consulta, para mostrar los resultados. Las vistas deben de tener 3 o más tablas de la BBDD.
 
 
+CREATE VIEW actor_film_list AS
+SELECT 
+    CONCAT(actor.first_name, ' ', actor.last_name) AS AN,
+    film.title as FT
+FROM film LEFT JOIN film_actor
+    ON film.film_id = film_actor.film_id 
+    LEFT JOIN actor
+    ON film_actor.film_id = actor.actor_id
+GROUP BY AN;
 
-¡
+CREATE VIEW top_ten AS
+SELECT 
+film.title as FT,
+category.name as CN,
+language.name as LN
+FROM film LEFT JOIN film_category
+ON film.film_id = film_category.film_id 
+LEFT JOIN category
+ON film_category.film_id  = category.category_id
+JOIN language
+ON film.film_id = language.language_id
+GROUP BY FT
+LIMIT 10;
